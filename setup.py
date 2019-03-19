da4579e4a81c0e4046544b25515c1c8f72b52e27
Trim21 <trim21me@gmail.com>
2019-03-19 19:00:40 +0800

    1.0.2

--- setup.py
+++ setup.py
@@ -3,26 +3,12 @@
 # Copyright (c) 2018 Trim21 <trim21me@hotmail.com>
 # Licensed under the MIT license.
 
-import textwrap
-
 from setuptools import setup
-
-
-def long_description():
-    with open('./README.md', 'rb') as f:
-        return f.read().decode('utf-8')
-
 
 setup(
     use_scm_version={
         "write_to": "transmission_rpc/__init__.py",
-        "write_to_template": textwrap.dedent(
-            """
-             # coding: utf-8
-             __version__ = {version!r}
-             """
-        ).lstrip(),
+        "write_to_template": '\n__version__ = {version!r}',
     },
-
     test_suite="test",
 )
--- transmission_rpc/__init__.py
+++ transmission_rpc/__init__.py
@@ -1,2 +1,15 @@
-# coding: utf-8
-__version__ = '1.0.1.dev3+g545c207.d20190319'
+# -*- coding: utf-8 -*-
+# Copyright (c) 2008-2014 Erik Svensson <erik.public@gmail.com>
+# Licensed under the MIT license.
+
+from transmission_rpc.client import Client
+from transmission_rpc.constants import DEFAULT_PORT, DEFAULT_TIMEOUT, PRIORITY, \
+    RATIO_LIMIT, LOGGER
+from transmission_rpc.error import TransmissionError
+from transmission_rpc.session import Session
+from transmission_rpc.torrent import Torrent
+
+__author__ = 'Trim21 <trim21me@hotmail.com>'
+__author_email__ = 'trim21me@hotmail.com'
+__copyright__ = 'Copyright (c) 2018 Trim21'
+__license__ = 'MIT'
