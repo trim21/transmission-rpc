0e2059b7c9c34745c3d9bebd5d2ea970d9cbde31
Trim21 <i@trim21.me>
2020-05-12 00:03:13 +0800

    tests: rewrite test case with pytest

--- tests/test_torrent.py
+++ tests/test_torrent.py
@@ -4,94 +4,88 @@
 
 import time
 import datetime
-import unittest
+
+import pytest
 
 import transmission_rpc
 import transmission_rpc.utils
 import transmission_rpc.constants
 
 
-class TestTorrent(unittest.TestCase):
-    def assertPropertyException(self, exception, object, property):
-        try:
-            getattr(object, property)
-        except exception:
-            pass
-        else:
-            self.fail()
+def test_initial():
+    with pytest.raises(ValueError):
+        transmission_rpc.Torrent(None, {})
+    transmission_rpc.Torrent(None, {'id': 42})
 
-    def testConstruction(self):
-        self.assertRaises(ValueError, transmission_rpc.Torrent, None, {})
-        transmission_rpc.Torrent(None, {'id': 42})
 
-    def testAttributes(self):
-        torrent = transmission_rpc.Torrent(None, {'id': 42})
-        self.assertTrue(hasattr(torrent, 'id'))
-        self.assertEqual(torrent.id, 42)
-        self.assertPropertyException(KeyError, torrent, 'status')
-        self.assertPropertyException(KeyError, torrent, 'progress')
-        self.assertPropertyException(KeyError, torrent, 'ratio')
-        self.assertPropertyException(KeyError, torrent, 'eta')
-        self.assertPropertyException(KeyError, torrent, 'date_active')
-        self.assertPropertyException(KeyError, torrent, 'date_added')
-        self.assertPropertyException(KeyError, torrent, 'date_started')
-        self.assertPropertyException(KeyError, torrent, 'date_done')
+def assert_property_exception(exception, ob, prop):
+    with pytest.raises(exception):
+        getattr(ob, prop)
 
-        self.assertRaises(KeyError, torrent.format_eta)
-        self.assertEqual(torrent.files(), {})
 
-        data = {
-            'id': 1,
-            'status': 4,
-            'sizeWhenDone': 1000,
-            'leftUntilDone': 500,
-            'uploadedEver': 1000,
-            'downloadedEver': 2000,
-            'uploadRatio': 0.5,
-            'eta': 3600,
-            'activityDate': time.mktime((2008, 12, 11, 11, 15, 30, 0, 0, -1)),
-            'addedDate': time.mktime((2008, 12, 11, 8, 5, 10, 0, 0, -1)),
-            'startDate': time.mktime((2008, 12, 11, 9, 10, 5, 0, 0, -1)),
-            'doneDate': time.mktime((2008, 12, 11, 10, 0, 15, 0, 0, -1)),
-        }
+def test_attributes():
+    torrent = transmission_rpc.Torrent(None, {'id': 42})
+    assert hasattr(torrent, 'id')
+    assert torrent.id == 42
+    assert_property_exception(KeyError, torrent, 'status')
+    assert_property_exception(KeyError, torrent, 'progress')
+    assert_property_exception(KeyError, torrent, 'ratio')
+    assert_property_exception(KeyError, torrent, 'eta')
+    assert_property_exception(KeyError, torrent, 'date_active')
+    assert_property_exception(KeyError, torrent, 'date_added')
+    assert_property_exception(KeyError, torrent, 'date_started')
+    assert_property_exception(KeyError, torrent, 'date_done')
 
-        torrent = transmission_rpc.Torrent(None, data)
-        self.assertEqual(torrent.id, 1)
-        self.assertEqual(torrent.leftUntilDone, 500)
-        self.assertEqual(torrent.status, 'downloading')
-        self.assertEqual(torrent.progress, 50.0)
-        self.assertEqual(torrent.ratio, 0.5)
-        self.assertEqual(torrent.eta, datetime.timedelta(seconds=3600))
-        self.assertEqual(torrent.date_active, datetime.datetime(2008, 12, 11, 11, 15, 30))
-        self.assertEqual(torrent.date_added, datetime.datetime(2008, 12, 11, 8, 5, 10))
-        self.assertEqual(torrent.date_started, datetime.datetime(2008, 12, 11, 9, 10, 5))
-        self.assertEqual(torrent.date_done, datetime.datetime(2008, 12, 11, 10, 0, 15))
+    with pytest.raises(KeyError):
+        torrent.format_eta()
+    assert torrent.files() == {}
 
-        self.assertEqual(torrent.format_eta(), transmission_rpc.utils.format_timedelta(torrent.eta))
+    data = {
+        'id': 1,
+        'status': 4,
+        'sizeWhenDone': 1000,
+        'leftUntilDone': 500,
+        'uploadedEver': 1000,
+        'downloadedEver': 2000,
+        'uploadRatio': 0.5,
+        'eta': 3600,
+        'activityDate': time.mktime((2008, 12, 11, 11, 15, 30, 0, 0, -1)),
+        'addedDate': time.mktime((2008, 12, 11, 8, 5, 10, 0, 0, -1)),
+        'startDate': time.mktime((2008, 12, 11, 9, 10, 5, 0, 0, -1)),
+        'doneDate': time.mktime((2008, 12, 11, 10, 0, 15, 0, 0, -1)),
+    }
 
-        torrent = transmission_rpc.Torrent(None, {'id': 42, 'eta': -1})
-        self.assertPropertyException(ValueError, torrent, 'eta')
+    torrent = transmission_rpc.Torrent(None, data)
+    assert torrent.id == 1
+    assert torrent.leftUntilDone == 500
+    assert torrent.status == 'downloading'
+    assert torrent.progress == 50.0
+    assert torrent.ratio == 0.5
+    assert torrent.eta == datetime.timedelta(seconds=3600)
+    assert torrent.date_active == datetime.datetime(2008, 12, 11, 11, 15, 30)
+    assert torrent.date_added == datetime.datetime(2008, 12, 11, 8, 5, 10)
+    assert torrent.date_started == datetime.datetime(2008, 12, 11, 9, 10, 5)
+    assert torrent.date_done == datetime.datetime(2008, 12, 11, 10, 0, 15)
 
-        data = {
-            'id': 1,
-            'status': 4,
-            'sizeWhenDone': 1000,
-            'leftUntilDone': 500,
-            'uploadedEver': 1000,
-            'downloadedEver': 2000,
-            'uploadRatio': 0.5,
-            'eta': 3600,
-            'activityDate': time.mktime((2008, 12, 11, 11, 15, 30, 0, 0, -1)),
-            'addedDate': time.mktime((2008, 12, 11, 8, 5, 10, 0, 0, -1)),
-            'startDate': time.mktime((2008, 12, 11, 9, 10, 5, 0, 0, -1)),
-            'doneDate': 0,
-        }
+    assert torrent.format_eta() == transmission_rpc.utils.format_timedelta(torrent.eta)
 
-        torrent = transmission_rpc.Torrent(None, data)
-        self.assertEqual(torrent.date_done, None)
+    torrent = transmission_rpc.Torrent(None, {'id': 42, 'eta': -1})
+    assert_property_exception(ValueError, torrent, 'eta')
 
-    def testUnicode(self):
-        torrent = transmission_rpc.Torrent(None, {'id': 42, 'name': 'あみ'})
-        self.assertEqual(torrent.id, 42)
-        repr(torrent)
-        str(torrent)
+    data = {
+        'id': 1,
+        'status': 4,
+        'sizeWhenDone': 1000,
+        'leftUntilDone': 500,
+        'uploadedEver': 1000,
+        'downloadedEver': 2000,
+        'uploadRatio': 0.5,
+        'eta': 3600,
+        'activityDate': time.mktime((2008, 12, 11, 11, 15, 30, 0, 0, -1)),
+        'addedDate': time.mktime((2008, 12, 11, 8, 5, 10, 0, 0, -1)),
+        'startDate': time.mktime((2008, 12, 11, 9, 10, 5, 0, 0, -1)),
+        'doneDate': 0,
+    }
+
+    torrent = transmission_rpc.Torrent(None, data)
+    assert torrent.date_done is None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,79 +2,92 @@
 # Copyright (c) 2018-2020 Trim21 <i@trim21.me>
 # Licensed under the MIT license.
 import datetime
-import unittest
+
+import pytest
 
 from transmission_rpc import utils
 
 
-class TestUtils(unittest.TestCase):
-    def testFormatSize(self):
-        table = {
-            512: (512, 'B'),
-            1024: (1.0, 'KiB'),
-            1048575: (1023.999, 'KiB'),
-            1048576: (1.0, 'MiB'),
-            1073741824: (1.0, 'GiB'),
-            1099511627776: (1.0, 'TiB'),
-            1125899906842624: (1.0, 'PiB'),
-            1152921504606846976: (1.0, 'EiB'),
-        }
-        for size, expected in table.items():
-            result = utils.format_size(size)
-            self.assertAlmostEqual(result[0], expected[0], 4)
-            self.assertEqual(result[1], expected[1])
+def assert_almost_eq(value: float, expected: float):
+    assert abs(value - expected) < 1
 
-    def testFormatSpeed(self):
-        table = {
-            512: (512, 'B/s'),
-            1024: (1.0, 'KiB/s'),
-            1048575: (1023.999, 'KiB/s'),
-            1048576: (1.0, 'MiB/s'),
-            1073741824: (1.0, 'GiB/s'),
-            1099511627776: (1.0, 'TiB/s'),
-            1125899906842624: (1.0, 'PiB/s'),
-            1152921504606846976: (1.0, 'EiB/s'),
-        }
-        for size, expected in table.items():
-            result = utils.format_speed(size)
-            self.assertAlmostEqual(result[0], expected[0], 4)
-            self.assertEqual(result[1], expected[1])
 
-    def testFormatTimedelta(self):
-        table = {
-            datetime.timedelta(0, 0): '0 00:00:00',
-            datetime.timedelta(0, 10): '0 00:00:10',
-            datetime.timedelta(0, 60): '0 00:01:00',
-            datetime.timedelta(0, 61): '0 00:01:01',
-            datetime.timedelta(0, 3661): '0 01:01:01',
-            datetime.timedelta(1, 3661): '1 01:01:01',
-            datetime.timedelta(13, 65660): '13 18:14:20',
-        }
-        for delta, expected in table.items():
-            self.assertEqual(utils.format_timedelta(delta), expected)
+@pytest.mark.parametrize(
+    'size,expected', {
+        512: (512, 'B'),
+        1024: (1.0, 'KiB'),
+        1048575: (1023.999, 'KiB'),
+        1048576: (1.0, 'MiB'),
+        1073741824: (1.0, 'GiB'),
+        1099511627776: (1.0, 'TiB'),
+        1125899906842624: (1.0, 'PiB'),
+        1152921504606846976: (1.0, 'EiB'),
+    }.items()
+)
+def test_format_size(size, expected):
+    result = utils.format_size(size)
+    assert_almost_eq(result[0], expected[0])
+    assert result[1] == expected[1]
 
-    def testFormatTimestamp(self):
-        table = {
-            0: '-',
-            1: '1970-01-01 00:00:01',
-            1129135532: '2005-10-12 16:45:32',
-        }
-        for timestamp, expected in table.items():
-            self.assertEqual(utils.format_timestamp(timestamp, utc=True), expected)
 
-    def testRPCBool(self):
-        table = {
-            0: 0,
-            1: 1,
-            1000: 1,
-            'true': 1,
-            'Yes': 1,
-            'truE': 1,
-            'baka': 0,
-            'false': 0,
-            'no': 0,
-            True: 1,
-            False: 0,
-        }
-        for value, expected in table.items():
-            self.assertEqual(utils.rpc_bool(value), expected)
+@pytest.mark.parametrize(
+    'size,expected', {
+        512: (512, 'B/s'),
+        1024: (1.0, 'KiB/s'),
+        1048575: (1023.999, 'KiB/s'),
+        1048576: (1.0, 'MiB/s'),
+        1073741824: (1.0, 'GiB/s'),
+        1099511627776: (1.0, 'TiB/s'),
+        1125899906842624: (1.0, 'PiB/s'),
+        1152921504606846976: (1.0, 'EiB/s'),
+    }.items()
+)
+def test_format_speed(size, expected):
+    result = utils.format_speed(size)
+    assert_almost_eq(result[0], expected[0])
+    assert result[1] == expected[1]
+
+
+@pytest.mark.parametrize(
+    'delta,expected', {
+        datetime.timedelta(0, 0): '0 00:00:00',
+        datetime.timedelta(0, 10): '0 00:00:10',
+        datetime.timedelta(0, 60): '0 00:01:00',
+        datetime.timedelta(0, 61): '0 00:01:01',
+        datetime.timedelta(0, 3661): '0 01:01:01',
+        datetime.timedelta(1, 3661): '1 01:01:01',
+        datetime.timedelta(13, 65660): '13 18:14:20',
+    }.items()
+)
+def test_format_timedelta(delta, expected):
+    assert utils.format_timedelta(delta), expected
+
+
+@pytest.mark.parametrize(
+    'timestamp, expected', {
+        0: '-',
+        1: '1970-01-01 00:00:01',
+        1129135532: '2005-10-12 16:45:32',
+    }.items()
+)
+def test_format_timestamp(timestamp, expected):
+    assert utils.format_timestamp(timestamp, utc=True) == expected
+
+
+@pytest.mark.parametrize(
+    'value, expected', {
+        0: 0,
+        1: 1,
+        1000: 1,
+        'true': 1,
+        'Yes': 1,
+        'truE': 1,
+        'baka': 0,
+        'false': 0,
+        'no': 0,
+        True: 1,
+        False: 0,
+    }.items()
+)
+def test_rpc_bool(value, expected):
+    assert utils.rpc_bool(value) == expected, f'{value} should be convert to {expected}'
