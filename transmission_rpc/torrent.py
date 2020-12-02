a9990c0aab3f07427e1872231d09ea6887a91a03
Nazar Vovk <nazarii.vovk@chatbots.studio>
2020-12-02 23:42:30 +0200

    fallback to old way if percentDone is not available

--- transmission_rpc/torrent.py
+++ transmission_rpc/torrent.py
@@ -233,8 +233,16 @@ class Torrent:
     @property
     def progress(self) -> float:
         """download progress in percent."""
-        # https://gist.github.com/jackiekazil/6201722#gistcomment-2788556
-        return round((100.0 * self._fields["percentDone"].value), 2)
+        try:
+            # https://gist.github.com/jackiekazil/6201722#gistcomment-2788556
+            return round((100.0 * self._fields["percentDone"].value), 2)
+        except KeyError:
+            try:
+                size = self._fields["sizeWhenDone"].value
+                left = self._fields["leftUntilDone"].value
+                return round((100.0 * (size - left) / float(size)), 2)
+            except ZeroDivisionError:
+                return 0.0
 
     @property
     def ratio(self) -> float:
