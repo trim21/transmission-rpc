4787114be6a47c6521fe120b1923e6720d078811
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-02-11 02:42:13 +0100

    feat: add additional properties (#51)

--- transmission_rpc/torrent.py
+++ transmission_rpc/torrent.py
@@ -370,6 +370,46 @@ class Torrent:
             raise ValueError("Not a valid limit")
 
     @property
+    def is_finished(self) -> bool:
+        """Returns true if the torrent is finished (available from rpc version 2.0)"""
+        return self._fields["isFinished"].value
+
+    @property
+    def is_stalled(self) -> bool:
+        """Returns true if the torrent is stalled (available from rpc version 2.4)"""
+        return self._fields["isStalled"].value
+
+    @property
+    def size_when_done(self) -> int:
+        """Size in bytes when the torrent is done"""
+        return self._fields["sizeWhenDone"].value
+
+    @property
+    def total_size(self) -> int:
+        """Total size in bytes"""
+        return self._fields["totalSize"].value
+
+    @property
+    def left_until_done(self) -> int:
+        """Bytes left until done"""
+        return self._fields["leftUntilDone"].value
+
+    @property
+    def desired_available(self) -> int:
+        """Bytes that are left to download and available"""
+        return self._fields["desiredAvailable"].value
+
+    @property
+    def available(self) -> float:
+        """Availability in percent"""
+        bytes_all = self.total_size
+        bytes_done = sum(
+            map(lambda x: x["bytesCompleted"], self._fields["fileStats"].value)
+        )
+        bytes_avail = self.desired_available + bytes_done
+        return (bytes_avail / bytes_all) * 100 if bytes_all else 0
+
+    @property
     def seed_idle_mode(self) -> str:
         """
         Seed idle mode as string. Can be one of 'global', 'single' or 'unlimited'.
