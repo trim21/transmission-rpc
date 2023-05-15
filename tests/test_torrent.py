aeed7143c12388f5fc35606b651000dccadac616
Trim21 <trim21.me@gmail.com>
2023-05-16 01:29:15 +0800

    use enum

--- tests/test_torrent.py
+++ tests/test_torrent.py
@@ -68,6 +68,7 @@ def test_attributes():
     assert torrent.id == 1
     assert torrent.left_until_done == 500
     assert torrent.status == "downloading"
+    assert torrent.status.downloading
     assert torrent.progress == 50.0
     assert torrent.ratio == 0.5
     assert torrent.eta == datetime.timedelta(seconds=3600)
