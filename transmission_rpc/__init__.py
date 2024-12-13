f8a5511980b76b66a35a2260dba338f65519dc9d
Trim21 <trim21.me@gmail.com>
2024-12-13 20:54:49 +0800

    merge

--- transmission_rpc/__init__.py
+++ transmission_rpc/__init__.py
@@ -1,8 +1,8 @@
 import logging
 import urllib.parse
 
-from transmission_rpc.client import Client
-from transmission_rpc.constants import DEFAULT_TIMEOUT, LOGGER, IdleMode, Priority, RatioLimitMode
+from transmission_rpc.client import DEFAULT_TIMEOUT, Client
+from transmission_rpc.constants import LOGGER, IdleMode, Priority, RatioLimitMode
 from transmission_rpc.error import (
     TransmissionAuthError,
     TransmissionConnectError,
@@ -51,6 +51,7 @@ def from_url(
         from_url("https://127.0.0.1/transmission/rpc")  # https://127.0.0.1:443/transmission/rpc
         from_url("http://127.0.0.1")  # http://127.0.0.1:80/transmission/rpc
         from_url("http://127.0.0.1/")  # http://127.0.0.1:80/
+        from_url("http+unix://%2Fvar%2Frun%2Ftransmission.sock/transmission/rpc")  # /transmission/rpc on /var/run/transmission.sock Unix socket
 
     Warnings:
         you can't ignore scheme, ``127.0.0.1:9091`` is not valid url, please use ``http://127.0.0.1:9091``
@@ -62,10 +63,16 @@ def from_url(
     u = urllib.parse.urlparse(url)
 
     protocol = u.scheme
+    host = u.hostname
+    default_port = None
     if protocol == "http":
         default_port = 80
     elif protocol == "https":
         default_port = 443
+    elif protocol == "http+unix":
+        if host is None:
+            raise ValueError("http+unix URL is missing Unix socket path")
+        host = urllib.parse.unquote(host, errors="strict")
     else:
         raise ValueError(f"unknown url scheme {u.scheme}")
 
@@ -73,7 +80,7 @@ def from_url(
         protocol=protocol,  # type: ignore
         username=u.username,
         password=u.password,
-        host=u.hostname or "127.0.0.1",
+        host=host or "127.0.0.1",
         port=u.port or default_port,
         path=u.path or "/transmission/rpc",
         timeout=timeout,
