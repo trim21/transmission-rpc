0d5a46891c38d7a106f8c9c9bc3f636c08cf8368
Trim21 <trim21.me@gmail.com>
2025-02-17 04:32:51 +0800

    fix

---
+++ transmission_rpc/utils.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+def format_timedelta(delta: datetime.timedelta) -> str:
+    """
+    Format datetime.timedelta into <days> <hours>:<minutes>:<seconds>.
+    """
+    minutes, seconds = divmod(delta.seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return f"{delta.days:d} {hours:02d}:{minutes:02d}:{seconds:02d}"
